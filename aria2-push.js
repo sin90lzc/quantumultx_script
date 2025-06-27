629ab1e99f7ab5edb8ce19ba873dd8d66062f6c7
sin90lzc <lzcgame@gmail.com>
2025-06-27 12:54:05 +0800

    Update aria2-push.js

--- aria2-push.js
+++ aria2-push.js
@@ -1,12 +1,32 @@
 // Quantumult X 脚本：推送匹配请求到 Aria2 下载器
 
 (async () => {
+  // 读取 URL 参数
+  function getQueryParams(url) {
+    const queryString = url.split("?")[1] || "";
+    return Object.fromEntries(queryString.split("&").map(p => p.split("=")));
+  }
   const url = $request.url;
+  const query = getQueryParams(url);
+  const series = query.series || "default";
+  const token = query.token || "";
 
   const aria2Host = "https://a2d.8v8.fun:4443/jsonrpc"; // ← 请替换
-  const token = "SpIwIwGdmp0dwgX";                          // ← 请替换为你的 Aria2 rpc-secret
-  const downloadDir = "/downloads/book/yueting_wulinchuanqi";           // ← Aria2 的保存目录（容器内路径）
-
+  
+  const downloadDir = "/downloads/book/"+series;           // ← Aria2 的保存目录（容器内路径）
+  
+  let episodeNumber = "000";
+  
+  try {
+    const res = await $task.fetch({
+      method: "GET",
+      url: `http://qx.local/counter?series=${series}`
+    });
+    const json = JSON.parse(res.body);
+    episodeNumber = String(json.next).padStart(3, "0");
+  } catch (e) {
+    console.log("❌ 获取集数编号失败：" + e);
+  }
   const json = {
     jsonrpc: "2.0",
     method: "aria2.addUri",
